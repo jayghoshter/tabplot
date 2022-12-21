54664d21dfe1bf565f05da5e50d10efbad398af2
Jayghosh Rao <jayghoshter@gmail.com>
2022-12-21 01:41:01 +0100

    more features in test

--- examples/test.py
+++ examples/test.py
@@ -3,10 +3,14 @@ from tplot import Plot
 plot = Plot()
 plot.files = ['./monodisperse']
 plot.twinx = ['./polydisperse']
+plot.linestyles = 'solid'
 plot.linewidths = 2
 plot.title = 'Title'
 plot.xlabel = 'x axis'
 plot.ylabel = 'y axis'
+plot.fit_lines = True
+plot.hlines = [ 3e-3, 4e-3 ]
+plot.vlines = [ 2000, 4000 ]
 
 plot.generate()
 plot.display()
