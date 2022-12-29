8224fc0a5aad60c9562aa7927aa85724f33a810b
Jayghosh Rao <jayghoshter@gmail.com>
2022-12-29 13:59:51 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -48,12 +48,14 @@
             ];
 
             shellHook = ''
-              # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
-              # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
-              export PYTHONPATH="$(pwd):$PYTHONPATH"
-              export PATH="$(pwd)/bin:$PATH"
-              unset SOURCE_DATE_EPOCH
-            '';
+                # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+                # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+                export PIP_PREFIX=$(pwd)/_build/pip_packages
+                export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+                export PYTHONPATH="$(pwd):$PYTHONPATH"
+                export PATH="$(pwd)/bin:$PATH"
+                unset SOURCE_DATE_EPOCH
+                '';
 
         };
       });
