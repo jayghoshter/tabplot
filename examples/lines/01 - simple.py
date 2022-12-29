194ecabb70189829c9c5a3ac9c41a69625b825b9
Jayghosh Rao <jayghoshter@gmail.com>
2022-12-29 14:04:27 +0100

    add examples

---
+++ examples/lines/01 - simple.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3 
+
+from tabplot import Plot
+
+# Initialize Plot object
+plot = Plot()
+
+# Set files and plot attributes/properties (matplotlib)
+plot.files = ['./line_a.tsv', './line_b.tsv']       # List of files to read
+plot.labels = ['A', 'B']                    # labels per file/line
+
+# Plot properties can be their primitive values (str/float) or a list of them.
+# If primitives are used, the same value is applied to all plot lines
+# Otherwise, the property for each plot line is taken from the given list
+plot.linewidths = 2  # or [2,2] etc
+
+plot.legend = ('upper left', 0, 1)
+
+# Read, draw and save the plot
+plot.read()
+plot.draw()
+plot.save('simple-1.pdf')
+plot.close()
+# # OR: use operator chaining to do the same
+# plot.read().draw().save('plot.pdf')
+
+# Initialize with kwargs
+Plot(
+    files = ['./line_b.tsv', 'line_a.tsv'],
+    labels = ['A', 'B'],
+    markers = 'D',
+    legend_size = 20,
+).read().draw().save('simple-2.pdf').close()
+
+# Initialize with dict
+attrs = {
+    'files': ['./line_a.tsv'],
+    'labels': ['The one true curve'],
+    'linestyles': 'dashdot',
+    'linewidths': 2,
+    'legend': ('upper left', 0, 1)
+}
+Plot(**attrs).read().draw().save('simple-3.pdf').close()
---
+++ examples/lines/02 - multi_draw.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3 
+
+from tabplot import Plot
+
+plot = Plot()
+plot.files = ['./line_a.tsv']
+plot.labels = ['A']
+plot.linewidths = 2
+plot.legend = ('upper left', 0, 1)
+plot.read().draw().save('multi-1.pdf')
+
+plot.files = ['./line_b.tsv']
+plot.labels = ['B']
+plot.line_color_indices = 1
+# NOTE: When clean=False, any previous rendered plot remains and we plot over it
+plot.read().draw(clean=False).save('multi-2.pdf')
+
+plot.files = ['./line_a.tsv', './line_b.tsv']
+plot.labels = ['A', 'B', 'C']
+plot.line_color_indices = []  # reset line_color_indices
+plot.read().draw().save('multi-3.pdf')
