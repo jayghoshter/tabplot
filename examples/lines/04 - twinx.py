72ff2f911c93b099c04db1aa15dff58a1e5aa473
Jayghosh Rao <jayghoshter@gmail.com>
2023-01-04 23:16:54 +0100

    twinx example

---
+++ examples/lines/04 - twinx.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from tabplot import Plot
+
+Plot(
+    files = ['./line_a.tsv'], 
+    twinx=['./line_b.tsv'] ,
+    labels = ['A', 'B'],
+    xlabel = 'X',
+    ylabel = 'Y',
+    y2label = 'Y2',
+    linewidths  =  2,
+).read().draw().show()
