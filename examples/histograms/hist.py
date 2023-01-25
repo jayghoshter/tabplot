676abaa15831ef3c603810c1fa672e1007b3c215
Jayghosh Rao <jayghoshter@gmail.com>
2023-01-25 21:48:18 +0100

    add histograms example

---
+++ examples/histograms/hist.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from tabplot import Histogram
+
+Histogram().read(files=['hist1.csv']).draw().save('hist1.pdf')
+
+Histogram().read(files=['hist1.csv', 'hist2.csv']).scale(y=1000000).draw().save('hist_both.pdf')
+
+Histogram(stacked=True).read(files=['hist1.csv', 'hist2.csv']).scale(y=1000000).draw().save('hist_both_stacked.pdf')
