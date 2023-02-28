10560071e247da8819fb00c133eae3b024a5bc2e
Jayghosh Rao <jayghoshter@gmail.com>
2023-02-28 22:51:25 +0100

    set default xticks to indices in bar plot

--- tabplot/barplot.py
+++ tabplot/barplot.py
@@ -1,5 +1,5 @@
 from tabplot import Plot
-
+import matplotlib.pyplot as plt
 
 class BarPlot(Plot):
     bar_width: float
@@ -11,9 +11,10 @@ class BarPlot(Plot):
     def _plot_data(self, ax, xs, ys, labels, zorders):
         lines = []
         bar_count = 0
+        indices = list(range(1, len(ys[0]) + 1))
+        num_files = len(ys)
+
         for x, y, label in zip(xs, ys, labels):
-            indices = list(range(1, len(y) + 1))
-            num_files = len(xs)
             width = self.bar_width
             shiftwidth = (num_files - 1) * width / 2.0
             position = [bar_count * width - shiftwidth + i for i in indices]
@@ -23,3 +24,46 @@ class BarPlot(Plot):
             # if args['xticks_column'] is not None:
             #     plt.xticks(indices,xticks)
         return lines
+
+    def _setup_ticks(
+        self, xticks=None, yticks=None, xtick_labels=None, ytick_labels=None
+    ):
+        if xticks is None:
+            xticks = self.xticks
+
+        if yticks is None:
+            yticks = self.yticks
+
+        if xtick_labels is None:
+            xtick_labels = self.xtick_labels
+
+        if ytick_labels is None:
+            ytick_labels = self.ytick_labels
+
+        xtx, xtl = plt.xticks()
+
+        # Use indices unless xticks/labels are explicitly provided
+        indices = list(range(1, len(self.ys[0]) + 1))
+
+        if len(xticks):
+            xtx = xticks
+        else: 
+            xtx = indices
+
+        if len(xtick_labels):
+            xtl = xtick_labels
+        else: 
+            xtl = [ str(x) for x in xtx ]
+
+        if len(xticks) or len(xtick_labels):
+            plt.xticks(xtx, xtl)
+
+        ytx, ytl = plt.yticks()
+
+        if len(yticks):
+            ytx = yticks
+        if len(ytick_labels):
+            ytl = ytick_labels
+
+        if len(yticks) or len(ytick_labels):
+            plt.yticks(ytx, ytl)
