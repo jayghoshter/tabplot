129da05e7ef928db9e7becb933d876892ed2d1cc
Jayghosh Rao <jayghoshter@gmail.com>
2023-05-28 14:37:15 +0200

    update bar plot example

--- examples/bars/bar2.py
+++ examples/bars/bar2.py
@@ -2,4 +2,20 @@
 
 from tabplot import BarPlot
 
-BarPlot(style='science', bar_width=1).read(files=['./commits-by-date.csv'], columns=(-1,1), xticklabels_column=0).draw().show()
+BarPlot(style='science', bar_width=0.5, xticklabels_rotation=45).read(files=['./commits-by-date.csv'], columns=(-1,1), xticklabels_column=0).draw().show()
+
+(
+    BarPlot(
+        style='science', 
+        bar_width=0.5, 
+        xticklabels_rotation=90,
+        strip_xticklabels=True,)
+    .read(
+        files=['./commits-by-date-2.csv'], 
+        columns=(-1,0), 
+        xticklabels_column=1)
+    .setup()
+    .setrc({'xtick.labelsize': 8})
+    .draw()
+    .show()
+    )
